c7f682ffe2638071600ce6deba60e70e37ac4926
SyamaPS <77504213+SyamaPS@users.noreply.github.com>
2022-03-29 11:38:26 +0530

    Add files via upload

---
+++ p2_display_details.java
@@ -0,0 +1,10 @@
+package com.example.syamaps;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class p2_display_details extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_p2_display_details);
+}
+}
