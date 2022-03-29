7491c032a3b764798c2ea5ced98f9fef0fa91a81
SyamaPS <77504213+SyamaPS@users.noreply.github.com>
2022-03-29 11:38:05 +0530

    Add files via upload

---
+++ p1_hello_world.java
@@ -0,0 +1,10 @@
+package com.example.syamaps;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class p1_hello_world extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_p1_hello_world);
+}
+}
